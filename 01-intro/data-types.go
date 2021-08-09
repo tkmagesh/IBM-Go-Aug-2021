683e984220d764c78e6a1880d2fb8952387df2d8
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-09 12:11:51 +0530

    variable declaration

---
+++ 01-intro/data-types.go
@@ -0,0 +1,88 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		var msg string
+		msg = "Hello World!"
+	*/
+
+	/*
+		var msg string = "Hello World!"
+	*/
+
+	//using type inference
+	/*
+		var msg = "Hello World!"
+	*/
+
+	//the following is applicable only in a function (not at the package level)
+	msg := "Hello World!"
+	fmt.Println(msg)
+
+	/*
+		var label string
+		var x int
+		var y int
+	*/
+
+	/*
+		var label string
+		var x, y int
+	*/
+
+	/*
+		var label string
+		var (
+			x int
+			y int
+		)
+	*/
+	/*
+		var (
+			label string
+			x     int
+			y     int
+		)
+	*/
+
+	/*
+		var (
+			label string
+			x, y  int
+		)
+
+		label = "Add Result :"
+		x = 100
+		y = 200
+	*/
+
+	/*
+		var (
+			label string = "Add Result :"
+			x     int    = 100
+			y     int    = 200
+		)
+	*/
+
+	/*
+		var (
+			label string = "Add Result :"
+			x, y  int    = 100, 200
+		)
+	*/
+
+	/*
+		var (
+			label, x, y = "Add Result :", 100, 200
+		)
+	*/
+
+	/*
+		var label, x, y = "Add Result :", 100, 200
+	*/
+
+	label, x, y := "Add Result :", 100, 200
+	fmt.Println(label, x+y)
+}
