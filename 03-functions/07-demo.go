3558fe151dd88851548e0a63e5c4dcfae9f91a6f
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-09 16:48:14 +0530

    counter assignment - solution

--- 03-functions/07-demo.go
+++ 03-functions/07-demo.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	/* implement the functions without using any package level variables */
+	increment, decrement := getCounter()
 	fmt.Println(increment()) //=> 1
 	fmt.Println(increment()) //=> 2
 	fmt.Println(increment()) //=> 3
@@ -14,4 +15,17 @@ func main() {
 	fmt.Println(decrement()) //=> 1
 	fmt.Println(decrement()) //=> 0
 	fmt.Println(decrement()) //=> -1
+}
+
+func getCounter() (func() int, func() int) {
+	counter := 0
+	increment := func() int {
+		counter++
+		return counter
+	}
+	decrement := func() int {
+		counter--
+		return counter
+	}
+	return increment, decrement
 }
