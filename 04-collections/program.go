3483f61fe155f3ec46eb5e02c5c6babf1b587efc
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-10 10:42:56 +0530

    Using maps

--- 04-collections/program.go
+++ 04-collections/program.go
@@ -51,4 +51,56 @@ func main() {
 	fmt.Println("[1:3] => ", names[1:3])
 	fmt.Println("[3:] => ", names[3:])
 	fmt.Println("[:3] => ", names[:3])
+
+	fmt.Printf("len(names) => %d, cap(names) => %d\n", len(names), cap(names))
+	names = append(names, "x", "y", "z")
+	fmt.Printf("names = %v, len(names) => %d, cap(names) => %d\n", names, len(names), cap(names))
+
+	list := make([]int, 5, 100)
+	fmt.Println(list, len(list), cap(list))
+	list[0] = 0
+	list[1] = 10
+	list[2] = 20
+	list[3] = 30
+	list[4] = 40
+
+	//list[5] = 50
+	list = append(list, 50)
+	fmt.Println(list, len(list), cap(list))
+	x := list[0:9]
+	fmt.Println(x)
+
+	//Maps
+	//key/value pair collection
+	fmt.Println("Maps")
+	var cityRanks map[string]int = map[string]int{
+		"Bengaluru": 4,
+		"Udupi":     1,
+		"Mangaluru": 2,
+		"Mysuru":    3,
+	}
+
+	fmt.Println(cityRanks)
+	rankOfMysuru := cityRanks["Mysuru"]
+	fmt.Println("Rank of Mysuru => ", rankOfMysuru)
+
+	//adding a new key/value pair
+	cityRanks["Pune"] = 5
+	fmt.Println(cityRanks)
+
+	//check if a key exists
+	if rank, ok := cityRanks["Ahmedabad"]; ok {
+		fmt.Println("Rank of Ahmedabad => ", rank)
+	} else {
+		fmt.Println("Ahmedabad is not present in the map")
+	}
+
+	//remove a key/value pair
+	delete(cityRanks, "Mangaluru")
+	fmt.Println("After removing mangaluru, cityRanks => ", cityRanks)
+
+	//iterate over map
+	for key, value := range cityRanks {
+		fmt.Printf("Value at %s is %d\n", key, value)
+	}
 }
