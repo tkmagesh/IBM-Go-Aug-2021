0ab4f8f32257c88fb6da48b160d915c3e8c71f90
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-12 17:49:49 +0530

    Update demo-10.go

--- 11-concurrency/demo-10.go
+++ 11-concurrency/demo-10.go
@@ -2,29 +2,33 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
-	go Print("Hello", 500*time.Millisecond, ch1, ch2)
-	go Print("world", 750*time.Millisecond, ch2, ch1)
+	wg := &sync.WaitGroup{}
+	ch1 := make(chan int, 1)
+	ch2 := make(chan int, 1)
+	wg.Add(2)
+	go Print("Hello", 500*time.Millisecond, ch1, ch2, wg)
+	go Print("world", 750*time.Millisecond, ch2, ch1, wg)
 	ch1 <- 1
-
+	wg.Wait()
 	/*
 		var input string
 		fmt.Scanln(&input)
 	*/
 }
 
-func Print(str string, delay time.Duration, ch1, ch2 chan int) {
+func Print(str string, delay time.Duration, ch1, ch2 chan int, wg *sync.WaitGroup) {
 	for i := 0; i < 5; i++ {
 		<-ch1
 		time.Sleep(delay)
 		fmt.Println(str)
 		ch2 <- 1
 	}
+	wg.Done()
 }
 
 /*
