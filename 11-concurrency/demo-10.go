fa123030006fe751db1724ea7f6616ea12f6921b
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-12 11:08:24 +0530

    Update demo-10.go

--- 11-concurrency/demo-10.go
+++ 11-concurrency/demo-10.go
@@ -8,11 +8,14 @@ import (
 func main() {
 	ch1 := make(chan int)
 	ch2 := make(chan int)
-	go Print("Hello", 2*time.Second, ch1, ch2)
-	go Print("world", 3*time.Second, ch2, ch1)
+	go Print("Hello", 500*time.Millisecond, ch1, ch2)
+	go Print("world", 750*time.Millisecond, ch2, ch1)
 	ch1 <- 1
-	var input string
-	fmt.Scanln(&input)
+
+	/*
+		var input string
+		fmt.Scanln(&input)
+	*/
 }
 
 func Print(str string, delay time.Duration, ch1, ch2 chan int) {
