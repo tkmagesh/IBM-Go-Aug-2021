961b7417fd07f2b12cd110fba8ca7ad7f2a14adb
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-12 16:17:25 +0530

    client streaming in gRPC

--- 13-grpc-app/client/client.go
+++ 13-grpc-app/client/client.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"grpc-app/proto"
+	"log"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -16,6 +18,35 @@ func main() {
 	defer client.Close()
 
 	clientConn := proto.NewAppServiceClient(client)
+	//doRequestResponse(clientConn)
+	doClientStreaming(clientConn)
+
+}
+
+//client streaming
+func doClientStreaming(clientConn proto.AppServiceClient) {
+	clientStream, err := clientConn.Average(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	nos := []int64{3, 1, 4, 2, 5}
+	for _, no := range nos {
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("Sending ", no)
+		req := &proto.AverageRequest{
+			No: no,
+		}
+		clientStream.Send(req)
+	}
+	res, err := clientStream.CloseAndRecv()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Average = ", res.GetResult())
+}
+
+//request & response
+func doRequestResponse(clientConn proto.AppServiceClient) {
 	req := &proto.AddRequest{
 		X: 100,
 		Y: 200,
--- 13-grpc-app/server/server.go
+++ 13-grpc-app/server/server.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"grpc-app/proto"
+	"io"
 	"log"
 	"net"
 
@@ -21,6 +23,30 @@ func (s *server) Add(ctx context.Context, req *proto.AddRequest) (*proto.AddResp
 		Result: result,
 	}
 	return res, nil
+}
+
+func (s *server) Average(stream proto.AppService_AverageServer) error {
+	var count int64
+	var sum int64
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			avg := sum / count
+			res := &proto.AverageResponse{
+				Result: avg,
+			}
+			stream.SendAndClose(res)
+			return nil
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		no := req.GetNo()
+		fmt.Println("Received ", no)
+		sum += no
+		count++
+	}
+	return nil
 }
 
 func main() {
